17d7176cf48c22e388ff98f4465c14456e597c15
lburdzy <lukaszburdzy@gmail.com>
2016-01-15 20:26:39 +0100

    added prezto config and little cleaning

--- systeminstall.sh
+++ systeminstall.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 yaourt -Sy
-yaourt -Su --needed neovim ipython ranger zsh tmux termite vlc redshift xclip ncdu htop tree meld
+yaourt -Su --needed neovim-git ipython ranger zsh tmux termite vlc redshift xclip ncdu htop tree meld tlp p7zip filezilla
