ede2aca675db82d978668832f7ca2af56f61042f
lburdzy <lukaszburdzy@gmail.com>
2016-01-28 23:48:49 +0100

    added fasd to systeminstall

--- systeminstall.sh
+++ systeminstall.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 yaourt -Sy
-yaourt -Su --needed neovim-git ipython ranger zsh tmux termite vlc redshift xclip ncdu htop tree meld tlp p7zip filezilla postgresql
+yaourt -Su --needed neovim-git ipython ranger zsh tmux termite vlc redshift xclip ncdu htop tree meld tlp p7zip filezilla postgresql fasd-git
