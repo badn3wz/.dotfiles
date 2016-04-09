156fc84b92e0970ab74d6765915dfe7f811607e1
lburdzy <lukaszburdzy@gmail.com>
2016-04-09 09:28:28 +0200

    dodany fish config

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
+set EDITOR nvim
+set VISUAL nvim
+# set -gx PATH ~/.gem/ruby/2.3.0/bin $PATH
+# set -g DJANGO_DATABASE default
+source ~/.config/fish/aliases.fish
+source ~/.config/fish/private_aliases.fish
--- systeminstall.sh
+++ systeminstall.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 yaourt -Sy
 yaourt -Su --needed neovim-git ipython ranger zsh tmux termite vlc redshift xclip ncdu htop tree meld tlp p7zip filezilla postgresql fasd-git
+mkdir ~/projects/ ~/clones/ ~/venvs/
