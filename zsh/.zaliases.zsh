9b2d7c843520e15b7e99965fca0a4d0955100b07
Łukasz Burdzy <lukasz.burdzy@ordergroup.pl>
2016-12-28 16:54:49 +0100

    Reworked aliases

--- zsh/.zaliases.zsh
+++ zsh/.zaliases.zsh
@@ -1,29 +1,51 @@
+# git
 alias gst="git status"
 alias gs="git status"
-alias dedyk="tmuxp load ~/.tmuxp/dedyk.yaml"
-alias yo="yaourt"
-alias yi="yaourt -S"
-alias ys="yaourt -Ss"
-alias yu="yaourt -Syua"
-alias clones="cd ~/clones"
-alias dot="cd ~/.dotfiles"
-alias lsd='ls --color=auto'
-alias mig="python manage.py migrate"
-alias mkmig="python manage.py makemigrations"
-alias nrc="nvim ~/.config/nvim/init.vim"
-alias proj="cd ~/projects"
-alias runs="python manage.py runserver_plus"
-alias zrc="nvim ~/.zshrc"
-alias zpr="nvim ~/.zpreztorc"
-alias zal="nvim ~/.zaliases.zsh"
-alias sp="python manage.py shell_plus"
+
+# python 
 alias ipy="ipython"
 alias ipy2="ipython2"
 alias pt="python -m pytest"
 alias cpt="coverage run -m pytest"
-alias asg="source ~/venvs/asg/bin/activate && cd ~/projects/asg/"
+
+# fzf
+alias ftr="freeze_to_file requirements.txt"
+alias ftf="freeze_to_file"
+alias ftrq="freeze_to_file req"
+
+# django
+alias mig="python manage.py migrate"
+alias mkmig="python manage.py makemigrations"
+alias runs="python manage.py runserver_plus"
+alias sp="python manage.py shell_plus"
+alias mng="python manage.py"
+
+# config files
+alias nrc="nvim ~/.config/nvim/init.vim"
+alias zrc="nvim ~/.zshrc"
+alias zpr="nvim ~/.zpreztorc"
+alias zal="nvim ~/.zaliases.zsh"
+
+# yaourt & pacman
+alias yo="yaourt"
+alias yi="yaourt -S"
+alias ys="yaourt -Ss"
+alias yu="yaourt -Syua"
+alias packey="add_pacman_key"
+
+# directories
+alias clones="cd ~/clones"
+alias dot="cd ~/.dotfiles"
+alias proj="cd ~/projects"
+
+# misc
+alias dedyk="tmuxp load ~/.tmuxp/dedyk.yaml"
+alias lsd='ls --color=auto'
 alias dlmp3="youtube-dl --extract-audio --audio-format mp3 --audio-quality 0"
+
+# projects
 alias pmsc="source ~/venvs/misc/bin/activate && cd ~/projects/"
+alias asg="source ~/venvs/asg/bin/activate && cd ~/projects/asg/"
 alias jr="source ~/venvs/jupyter/bin/activate && cd ~/projects/jupyter"
 alias img="source ~/projects/imgur/venv/bin/activate && cd ~/projects/imgur"
 alias arm="source ~/projects/arma/armaenv/bin/activate && cd ~/projects/arma/code"
@@ -32,6 +54,9 @@ alias bgm="source ~/projects/boardgames/zsenv/bin/activate && ~/projects/boardga
 alias obey="source ~/projects/obey/obeyenv/bin/activate && ~/projects/obey/superlists"
 alias red="source ~/projects/reddit/red/bin/activate && cd ~/projects/reddit/source"
 
+# ================================================================================================= #
+# functions                                                                                         #
+# ================================================================================================= #
 freeze_to_file () {
     if [ -z $1 ]
     then
@@ -44,13 +69,9 @@ freeze_to_file () {
         echo "File does not exist!"
     fi
 }
-alias ftr="freeze_to_file requirements.txt"
-alias ftf="freeze_to_file"
-alias ftrq="freeze_to_file req"
 
 add_pacman_key() {
     sudo pacman-key -r $1
     sudo pacman-key -f $1
     sudo pacman-key --lsignk-key $1
 }
-alias packey="add_pacman_key
